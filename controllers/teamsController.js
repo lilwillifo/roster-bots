853b0d115e9f699f086dc14d4d2ff1f9c37e1805
Margaret Williford <margaretwilliford4@gmail.com>
2018-08-11 13:42:34 -0600

    dummy data passing test

--- controllers/teamsController.js
+++ controllers/teamsController.js
@@ -3,9 +3,22 @@ const Team = require('../models/team')
 
 class TeamsController {
 
-  static index(request, response, next){
-    response.sendStatus(200)
+  static generate(request, response, next){
+    response.send({"totalTeamScore": TeamsController.totalTeamScore(),
+                    "starters": TeamsController.starters(),
+                    "substitutes": TeamsController.substitutes()})
   }
+
+  static totalTeamScore() {
+      return ``
+    }
+  static starters() {
+      return [1,2,3,4,5,6,7,8,9,0]
+    }
+  static substitutes() {
+      return [1,2,3,4,5]
+    }
+
 
 }
 
