39d011d6e91875aadbf614004ba2bb556d441d6b
Margaret Williford <margaretwilliford4@gmail.com>
2018-08-11 19:35:33 -0600

    refactor makeRoster method

--- js/rosterApp.js
+++ js/rosterApp.js
@@ -1,28 +1,32 @@
 //value fixed for purposes of this assignment and its assumptions
-const playerSalaryCap = 100;
-
-let makeRoster = (teamSize, substitutes, salaryCap) => {
+let makeRoster = (teamSize, starters, salaryCap) => {
   let roster = [];
 
   if (!teamSize || teamSize < 1) teamSize = 15;
-  if (!substitutes || substitutes >= teamSize) substitutes = 5;
+  if (!starters || starters >= teamSize) starters = 10;
   if (!salaryCap) salaryCap = 175;
 
-  let starters = teamSize - substitutes;
+  let substitutes = teamSize - starters;
   let starterPoints;
 
+  // This design assumes you only care about the starters,
+  // and are willing to have lowest scores for subs
   for (let i = 0; i < substitutes; i++) {
     roster.push(i);
   }
-  roster.length === 0 ? starterPoints = salaryCap : starterPoints = salaryCap - roster.reduce((a, b) => a + b);
+  starterPoints = salaryCap - roster.reduce((a, b) => a + b);
 
   while (starters > 0) {
-    roster.push(Math.ceil(starterPoints / teamSize));
+    // divides remaining points among starters
+    roster.push(Math.ceil(starterPoints / starters));
+
+    // ensures that there are no duplicated scores
     roster.sort();
     roster.forEach((element, index, array) => {
       if (array[index - 1] == element) roster[index] += 1;
     });
 
+    // reduces the remaining number of starterPoints to divy up
     starterPoints = salaryCap - roster.reduce((a, b) => a + b);
     starters -= 1;
   }
