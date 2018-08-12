cdec401a0ac85c24542f1409f85a865bc99144ba
Margaret Williford <margaretwilliford4@gmail.com>
2018-08-12 11:50:48 -0600

    styling fix for readability

--- controllers/teamsController.js
+++ controllers/teamsController.js
@@ -2,14 +2,12 @@ pry = require('pryjs');
 const Team = require('../models/team')
 
 class TeamsController {
-
   static generate(request, response, next){
     response.send({"totalTeamScore": Team.prototype.totalTeamScore(),
                     "starters": Team.prototype.starters(),
-                    "substitutes": Team.prototype.substitutes()})
+                    "substitutes": Team.prototype.substitutes()
+                  });
   }
-
-
 }
 
 module.exports = TeamsController
